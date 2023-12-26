777aa9bff40cd75fd8737549cba9a6ad31d3b232
DESKTOP-O4R0P87\Joanah <athrun.zala009@gmail.com>
2023-12-26 15:32:17 +0800

    added search on useServant hooks

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useServants, ServantSort } from "../../hooks/useServants";
 
 const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResults, setSearchResults] = useState<ServantSort[]>([]);
+
+  const handleSearch = async () => {
+    try {
+      const results = await useServants(searchValue);
+      setSearchResults(results);
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  };
+
+  console.log(searchValue, searchResults);
   return (
-    <InputGroup>
-      <InputLeftElement />
-      <Input
-        borderRadius={20}
-        placeholder="Search"
-        variant="filled"
-        isDisabled
-      />
-    </InputGroup>
+    <>
+      <InputGroup>
+        <InputLeftElement />
+        <Input
+          borderRadius={20}
+          placeholder="Search"
+          variant="filled"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
+        />
+      </InputGroup>
+    </>
   );
 };
 
--- src/hooks/useServants.ts
+++ src/hooks/useServants.ts
@@ -8,9 +8,19 @@ export interface ServantSort {
   face: string;
 }
 
-export const useServants = () => {
+export const useServants = async (searchName?: string): Promise<ServantSort[]> => {
   try {
-    return servantsData as unknown as ServantSort[];
+    if (searchName) {
+      // If searchName is provided, make an API call for searching by name
+      const apiUrl = `https://api.atlasacademy.io/nice/NA/servant/search?name=${encodeURIComponent(searchName)}`;
+      const response = await fetch(apiUrl);
+      const searchData = await response.json();
+      // Assuming the API response has a structure similar to ServantSort
+      return searchData as ServantSort[];
+    } else {
+      // If searchName is not provided, return the static data
+      return servantsData as unknown as ServantSort[];
+    }
   } catch (error) {
     throw new Error(`Failed to fetch servants.`);
   }

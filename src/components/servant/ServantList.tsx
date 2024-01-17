70a375ea79f370cc72e13dc376617588a46df235
DESKTOP-O4R0P87\Joanah <athrun.zala009@gmail.com>
2024-01-17 20:45:10 +0800

    added scroll restoration

--- src/components/servant/ServantList.tsx
+++ src/components/servant/ServantList.tsx
@@ -14,71 +14,72 @@ const ServantList = ({
   onServantClick,
   detailedServantData,
 }: ServantListProps) => {
+  const [initialCard, setInitialCard] = useState(20);
   const sentinelRef = useRef<HTMLDivElement>(null);
-  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // Save scroll position
-    const handleBeforeUnload = () => {
-      localStorage.setItem("scrollPosition", String(window.scrollY));
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
-  useEffect(() => {
-    // Restore scroll position
-    const savedScrollPosition = localStorage.getItem("scrollPosition");
-
-    if (savedScrollPosition) {
-      window.scrollTo(0, parseInt(savedScrollPosition, 10));
+  const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setInitialCard((prev) => prev + 20);
     }
-  }, []);
+  };
 
   useEffect(() => {
-    const intersectionObserver = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting && !loading) {
-          // Load more items when sentinel is in view
-          setLoading(true);
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
 
-          // Simulate loading data (replace this with actual data fetching)
-          setTimeout(() => {
-            // Set loading to false to enable loading more items again
-            setLoading(false);
-          }, 1000);
-        }
-      },
-      { threshold: 1 }
-    );
+    const observer = new IntersectionObserver(handleIntersection, options);
 
     if (sentinelRef.current) {
-      intersectionObserver.observe(sentinelRef.current);
+      observer.observe(sentinelRef.current);
     }
 
     return () => {
       if (sentinelRef.current) {
-        intersectionObserver.unobserve(sentinelRef.current);
+        observer.unobserve(sentinelRef.current);
       }
     };
-  }, [loading]);
+  }, []);
+
+  // useEffect(() => {
+  //   // Save scroll position
+  //   const handleBeforeUnload = () => {
+  //     localStorage.setItem("scrollPosition", String(window.scrollY));
+  //   };
+
+  //   window.addEventListener("beforeunload", handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   // Restore scroll position
+  //   const savedScrollPosition = localStorage.getItem("scrollPosition");
+
+  //   if (savedScrollPosition) {
+  //     window.scrollTo(0, parseInt(savedScrollPosition, 10));
+  //   }
+  // }, []);
 
   return (
     <>
       {servant && servant.length > 0
-        ? servant.map((item) => (
-            <ServantCard
-              key={item.id}
-              servant={item}
-              onClick={() => onServantClick(item.id)}
-              detailedServantData={detailedServantData}
-            />
-          ))
-        : Array.from({ length: 20 }, (_, index) => (
+        ? servant
+            // .slice(0, initialCard)
+            .map((item) => (
+              <ServantCard
+                key={item.id}
+                servant={item}
+                onClick={() => onServantClick(item.id)}
+                detailedServantData={detailedServantData}
+              />
+            ))
+        : Array.from({ length: initialCard }, (_, index) => (
             <Box key={index} boxShadow="md" height="85px" bg="whiteAlpha.100">
               <SkeletonText mt={4} px={3} noOfLines={3} skeletonHeight="3" />
             </Box>
